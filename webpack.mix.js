dc8c41ad3d17d9de785f8de952d7f93f169171ab
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-06-24 13:48:31 +0200

    Install LaravelMix and initialize structure

---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+// webpack.mix.js
+let mix = require('laravel-mix');
+mix
+.js('src/app.js', 'js')
+.sass('src/app.scss', 'css').options({ processCssUrls: false })
+.copyDirectory('src/img', 'dist/img')
+.setPublicPath('dist');
